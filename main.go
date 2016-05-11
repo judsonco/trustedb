8368fcc79546d8c8f2c5dce791a1999f78a48fe5
Jud <Jud.Stephenson@gmail.com>
2016-05-11 19:04:58 -0400

    Remove test docopt string

--- main.go
+++ main.go
@@ -709,26 +709,6 @@ func keyFromKeyFile(path string) (*btcec.PrivateKey, error) {
 }
 
 func main() {
-	usage := `Trustedb.
-Usage:
-  trustedb [--version] [--keyfile=<path>] [--trustfile=<path>]
-           <command> [<args>...]
-
-  options:
-    -h, --help
-
-  The most commonly used trustedb commands are:
-    init      Create a Trustfile
-    key       Show or Create your trustedb key
-    add       Add a key to the Trustfile
-    remove    Remove a key from the Trustfile
-    confirm   Confirm a Trustfile addition or removal
-
-  See 'trustedb help <command>' for more information on a specific command.`
-
-	arguments, _ := docopt.Parse(usage, nil, true, "Trustedb 0.0.1", true)
-	fmt.Println(arguments)
-
 	cp := cli.App("trustedb", "Trustedb.")
 	cp.String(StringOpt{
 		Name:   "k keyfile",
